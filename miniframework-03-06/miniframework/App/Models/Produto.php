364106e058d87c42fe9700c4e9950a9c17d5fda5
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2023-03-08 18:50:24 -0300

    Arquivo do dia 06/03/2023
    projeto feito no dia 06/03

---
+++ miniframework-03-06/miniframework/App/Models/Produto.php
@@ -0,0 +1,12 @@
+<?php
+    namespace App\Models;
+    use MF\Model\Model;
+    class Produto extends Model{
+    
+        public function getProdutos(){
+            //return array('Caderno','Mesa','Cadeira');
+            $query = "select id, descricao, preco from tb_produtos";
+            return $this->db->query($query)->fetchAll();
+        }
+    }
+?>
